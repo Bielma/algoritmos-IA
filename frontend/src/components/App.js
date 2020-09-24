74be3bfe4d61f9369aa5b7edb170166bec34a894
Antonio <thetonybl@gmailc.om>
2020-09-24 18:59:41 -0500

    graficar error con reschart

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,29 +1,33 @@
 import React, { useState } from 'react';
 import '../App.css';
-import Perceptron from '../hoocks/Perceptron';
+import Perceptron from '../hooks/Perceptron.js';
 //import usePerceptron from '../hoocks/usePerceptron';
-
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 function App() { 
 
   const [salida, setSalida ]= useState("");
   const [perceptron, setPerceptron] = useState(null);
-  
-
+  const [entrenado, setEntrenado] = useState(false);
   const iniciarPesos = () =>{
+    setEntrenado(false);
      setPerceptron( new Perceptron(4, 0.01, 1000));
   }
   const entrenar = () =>{
+    setEntrenado(false);
     perceptron.fit();
-    console.log(perceptron.w);
+    setEntrenado(true);
+    console.log(perceptron.errorAcumulado);
   }
   const probar = () =>{
+
     const x = [4, 1, 3];
     const xd = perceptron.predict(x);
     console.log("Salida perceptron :", xd);
     setSalida(xd);
     
   }
-
+  
+ 
   return (
     <div className="App">
       <header className="App-header">        
@@ -35,6 +39,15 @@ function App() {
 
         <input type="submit" value="Probar" onClick  = {probar}/>
         <a >{salida}</a>
+
+        { entrenado &&
+          <LineChart width={600} height={300} data={perceptron.errorAcumulado}>          
+            <Line type="monotone" dataKey="error" stroke="#8884d8" />
+              <CartesianGrid stroke="#ccc" />
+              <XAxis dataKey="epoca" />
+              <YAxis />
+          </LineChart>
+        }
       </header>
     </div>
   );
