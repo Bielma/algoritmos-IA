af65030325fb0542e35522b1a04fda038a5a6679
Antonio <thetonybl@gmailc.om>
2020-09-28 01:01:03 -0500

    Updates

---
+++ precache-manifest.705a63122463f5c5d79507022aa2f714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c522ebcbc8d91b6d6af11979bcc86b9",
+    "url": "/algoritmos-IA/index.html"
+  },
+  {
+    "revision": "849b02c98184cd63c5eb",
+    "url": "/algoritmos-IA/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4d48b81811d312d13f84",
+    "url": "/algoritmos-IA/static/css/main.bca7b399.chunk.css"
+  },
+  {
+    "revision": "849b02c98184cd63c5eb",
+    "url": "/algoritmos-IA/static/js/2.72b881f4.chunk.js"
+  },
+  {
+    "revision": "5788e7c91115777d2b6acaf02a1ddce1",
+    "url": "/algoritmos-IA/static/js/2.72b881f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d48b81811d312d13f84",
+    "url": "/algoritmos-IA/static/js/main.f846e1cd.chunk.js"
+  },
+  {
+    "revision": "7df6b5f11aa26621b087",
+    "url": "/algoritmos-IA/static/js/runtime-main.8eef04b2.js"
+  }
+]);
