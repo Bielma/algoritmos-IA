0fba5ccfcf0a255429ce688ae65a69512480a97e
bielma <thetonybl@gmail.com>
2021-04-16 16:31:47 -0500

    cbx para elejir funcion

--- frontend/src/components/PerceptronConfigs.js
+++ frontend/src/components/PerceptronConfigs.js
@@ -1,4 +1,4 @@
-import React,{useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { Form, FormControl } from 'react-bootstrap';
 
@@ -6,34 +6,35 @@ import { Button, FormControlLabel, Radio, RadioGroup, TextField } from '@materia
 import Perceptron from '../hooks/Perceptron.js';
 import { PerceptronContext } from "./PerceptronContext.js";
 import Adaline from '../algoritmos/Adaline.js';
-import Both from "../algoritmos/Both.js"
+import {sigmoidal, tangente, relu} from "../algoritmos/FuncionesActivacion"
 
 
-const perceptronTypes = [
+
+const funtionsTypes = [
     {
-        label: "Perceptron",
-        value: "perceptron"
+        label: "Sigmoidal",
+        value: "sigmoidal"
     },
     {
-        label: "Adaline",
-        value: "adaline"
+        label: "Tangente Hiperbolica",
+        value: "tangente"
     },
-    /*{
-        label: "Ambos",
-        value: "both"
-    }*/
+    {
+        label: "Lineal",
+        value: "lineal"
+    }
+
 ]
 
-const PerceptronConfigs = (props) =>  {    
-    //const [perceptron, setPerceptron] = useState(null);
-    //const [entrenado, setEntrenado] = useState(false);    
+const PerceptronConfigs = (props) => {
+    
     const { handleSubmit, register, errors, control, watch } = useForm();
-    const {perceptronState, setPerceptronState} = useContext(PerceptronContext);
+    const { perceptronState, setPerceptronState } = useContext(PerceptronContext);
     const [perceptronErrors, setPerceptronErrors] = useState({});
 
     const type = watch("type");
-    
-    const iniciarPesos = async (values) =>{
+
+    const iniciarPesos = async (values) => {
         console.log(values);
         let perceptron;
         setPerceptronErrors({});
@@ -45,63 +46,40 @@ const PerceptronConfigs = (props) =>  {
             });
             return;
         }
-        switch(type) {
-            case "perceptron":
-                perceptron = new Perceptron(perceptronState.x[0].length, values.learning_rate, values.max_epic_number, perceptronState.cpDrawer);
-                break;
-            case "adaline":
-                perceptron = new Adaline(
-                    perceptronState.x[0].length,             
-                    values.max_epic_number,
-                    values.max_error,
-                    values.learning_rate,
-                    perceptronState.cpDrawer
-                );
-                break;
-            case "both":
-                console.log("Ambos")
-                perceptron = new Both(
-                    perceptronState.x[0].length,             
-                    values.max_epic_number,
-                    values.max_error,
-                    values.learning_rate,
-                    perceptronState.cpDrawer
-                );
-                break;
-            default:
-                perceptron = new Adaline(
-                    perceptronState.x[0].length,             
-                    values.max_epic_number,
-                    values.max_error,
-                    values.learning_rate,
-                    perceptronState.cpDrawer
-                );
-        }
+        perceptron = new Adaline(
+            perceptronState.x[0].length,
+            values.max_epic_number,
+            values.max_error,
+            values.learning_rate,
+            perceptronState.cpDrawer,
+            (type==='sigmoidal') ? sigmoidal : (type==='tangente') ? tangente : relu
+        );
+        
 
-        setPerceptronState( {
+        setPerceptronState({
             ...perceptronState,
             perceptron,
-        
-        });       
-        const x2 = []; 
+
+        });
+        const x2 = [];
         x2[0] = perceptron.calcularX2(-5);
         x2[1] = perceptron.calcularX2(5);
         console.log("x2: ", x2);
         perceptronState.cpDrawer.clearCanvas();
         perceptronState.cpDrawer.drawAxis();
-        perceptronState.x.forEach ((point, index) => {
-            perceptronState.cpDrawer.drawPoint(perceptronState.cpDrawer.XC(point[0]), perceptronState.cpDrawer.YC(point[1]), perceptronState.y[index])             
-        });          
-        if(type === 'adaline'){
-            perceptronState.cpDrawer.drawLine(-5,x2[0],5,x2[1], "#FF0040");
-        }else{
-            perceptronState.cpDrawer.drawLine(-5, x2[0],5, x2[1], "#0101DF" );
-        }
+        perceptronState.x.forEach((point, index) => {
+            perceptronState.cpDrawer.drawPoint(perceptronState.cpDrawer.XC(point[0]), perceptronState.cpDrawer.YC(point[1]), perceptronState.y[index])
+        });
         
+            perceptronState.cpDrawer.drawLine(-5, x2[0], 5, x2[1], "#FF0040");
+        
+        //    perceptronState.cpDrawer.drawLine(-5, x2[0], 5, x2[1], "#0101DF");
         
 
-      }
-      const entrenar = async () =>{     
+
+
+    }
+    const entrenar = async () => {
         setPerceptronErrors({});
         if (!perceptronState.perceptron) {
             setPerceptronErrors({
@@ -110,30 +88,30 @@ const PerceptronConfigs = (props) =>  {
                 }
             });
             return;
-        }           
-        await perceptronState.perceptron.fit(perceptronState.x, perceptronState.y);     
-        const xd = perceptronState.perceptron.errorAcumulado.length >= perceptronState.perceptron.iterations;      
-        setPerceptronState( {
+        }
+        await perceptronState.perceptron.fit(perceptronState.x, perceptronState.y);
+        const xd = perceptronState.perceptron.errorAcumulado.length >= perceptronState.perceptron.iterations;
+        setPerceptronState({
             ...perceptronState,
             entrenado: true,
             limiteAlcanzado: xd
-        });           
+        });
         console.log(perceptronState.perceptron.w);
     }
 
-    const reiniciar = () =>{
+    const reiniciar = () => {
         perceptronState.cpDrawer.clearCanvas();
         perceptronState.cpDrawer.drawAxis();
-        setPerceptronState( {
+        setPerceptronState({
             ...perceptronState,
-            perceptron : null,
+            perceptron: null,
             entrenado: false,
-            x : [],
-            y : [],                                    
-        });           
+            x: [],
+            y: [],
+        });
     }
-      
-  
+
+
 
     return <>
         <Form onSubmit={handleSubmit(iniciarPesos)} className="">
@@ -146,16 +124,16 @@ const PerceptronConfigs = (props) =>  {
                 label="Nivel de aprendizaje"
                 rules={{
                     required: "Este campo es requerido",
-                    validate: value => (parseFloat(value, 10) > 0 && parseFloat(value, 10) <= 1)  || "El valor debe ser entre 0 y 1",
+                    validate: value => (parseFloat(value, 10) > 0 && parseFloat(value, 10) <= 1) || "El valor debe ser entre 0 y 1",
                 }}
                 helperText={errors?.learning_rate?.message}
                 error={!!errors?.learning_rate}
-                defaultValue = {0.1}
+                defaultValue={0.1}
                 margin="normal"
             />
             <br />
             <Controller
-                defaultValue ={50}
+                defaultValue={100}
                 as={TextField}
                 name="max_epic_number"
                 control={control}
@@ -168,25 +146,24 @@ const PerceptronConfigs = (props) =>  {
                 margin="normal"
             />
             <br />
-            {
-                (type === "adaline" || type === "both") &&
-                <Controller
-                    defaultValue ={0.01}
-                    as={TextField}
-                    name="max_error"
-                    control={control}
-                    id="max_error"
-                    name="max_error"
-                    label="Error"
-                    rules={{ required: "Este campo es requerido" }}
-                    helperText={errors?.max_error?.message}
-                    error={!!errors?.max_error}
-                    margin="normal"
-                />
-            }
-           
+
             <Controller
-                defaultValue ={"perceptron"}
+                defaultValue={0.01}
+                as={TextField}
+                name="max_error"
+                control={control}
+                id="max_error"
+                name="max_error"
+                label="Error"
+                rules={{ required: "Este campo es requerido" }}
+                helperText={errors?.max_error?.message}
+                error={!!errors?.max_error}
+                margin="normal"
+            />
+
+
+            <Controller
+                defaultValue={"sigmoidal"}
                 as={RadioGroup}
                 name="type"
                 control={control}
@@ -198,7 +175,7 @@ const PerceptronConfigs = (props) =>  {
                 margin="normal"
             >
                 {
-                    perceptronTypes.map((type, index) => 
+                    funtionsTypes.map((type, index) =>
                         <FormControlLabel
                             value={type.value}
                             key={index}
@@ -217,9 +194,10 @@ const PerceptronConfigs = (props) =>  {
                     )
                 }
             </Controller>
+
             <br />
 
-        
+
 
             {
                 perceptronErrors.trainingSet &&
@@ -230,16 +208,16 @@ const PerceptronConfigs = (props) =>  {
                 perceptronErrors.trainedPerceptron &&
                 <span className="error">{perceptronErrors.trainedPerceptron.message}</span>
             }
-            
-            <Button className="mt-4" type="sumbit" fullWidth color="primary" style={{color: "#03A9F4"}}>  Inicializar </Button>
-            
+
+            <Button className="mt-4" type="sumbit" fullWidth color="primary" style={{ color: "#03A9F4" }}>  Inicializar </Button>
+
         </Form>
-        <Form onSubmit={handleSubmit(entrenar)} className="">                        
-            <Button className="mt-4" type="sumbit" fullWidth color="primary" style={{color: "#03A9F4"}}>Entrenar</Button>
+        <Form onSubmit={handleSubmit(entrenar)} className="">
+            <Button className="mt-4" type="sumbit" fullWidth color="primary" style={{ color: "#03A9F4" }}>Entrenar</Button>
         </Form>
-        
-        <Form onSubmit={handleSubmit(reiniciar)} className="">                        
-            <Button className="mt-4" type="sumbit" fullWidth color="primary" style={{color: "#03A9F4"}}>Reiniciar</Button>
+
+        <Form onSubmit={handleSubmit(reiniciar)} className="">
+            <Button className="mt-4" type="sumbit" fullWidth color="primary" style={{ color: "#03A9F4" }}>Reiniciar</Button>
         </Form>
 
     </>
