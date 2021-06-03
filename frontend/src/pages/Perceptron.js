c74f832a2b386372cf07d736ecd276dbd098bcf5
Antonio <thetonybl@gmailc.om>
2021-06-03 09:07:29 -0500

    plano cartesiano removido

--- frontend/src/pages/Perceptron.js
+++ frontend/src/pages/Perceptron.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState, Component} from "react";
+import React, { useContext, useState, Component } from "react";
 import { Container, Row, Col } from 'react-bootstrap';
 
 import CartesianPlane from '../components/CartesianPlane';
@@ -7,73 +7,49 @@ import ErrorChart from '../components/ErrorChart';
 import { PerceptronContext } from "../components/PerceptronContext";
 import Alert from '../components/Alert.js';
 const Perceptron = (props) => {
-    const {perceptronState} = useContext(PerceptronContext); 
+    const { perceptronState } = useContext(PerceptronContext);
     return <>
         <Container fluid>
-            
-            <Row>
+
+            {
+                perceptronState.perceptron &&
+                <Row>
                 <Col
-                    md={{span: 7}}
-                    sm={{span: 12}}
+                    md={{ span: 12 }}
+                    sm={{ span: 12 }}
                 >
                     <div className="card card--box">
                         <div className="card__header">
-                            Plano cartesiano
+                            Grafica                        </div>
+                        <div style={{ width: '100%', height: 500, paddingBottom: '11%' }}>
+                            {
+
+                                <ErrorChart />
+                            }
+
                         </div>
-                        <CartesianPlane />
                     </div>
                 </Col>
-                <Col 
-                    md={{span: 5}}
-                    sm={{span: 12}}
+
+            </Row>}
+            <Row>
+                <Col
+                    md={{ span: 5 }}
+                    sm={{ span: 12 }}
                 >
                     <div className="card card--box">
                         <div className="card__header">
                             Configuraciones
                         </div>
-                        <PerceptronConfigs />                       
+                        <PerceptronConfigs />
                     </div>
-                    {  
-                        perceptronState.entrenado &&         
-                        <Row>
-                            <Col
-                                md={{span: 12}}
-                                sm={{span: 5}}
-                                >
-                                <div className="card card--box">
-                                    
-                                    <div style={{ width: '100%', height: 75 }}>
-                                        <Alert />
-                                    </div>
-                                </div>
-                            </Col>
-                        </Row>                                                            
-                    }
+
+
                 </Col>
             </Row>
-           
-            {
-                perceptronState.entrenado && 
-                <Row>
-                    <Col sm={{span: 12}}>
-                        <div className="card card--box">
-                            <div className="card__header">
-                                Grafica
-                            </div>
-                            <div style={{ width: '100%', height: 500, paddingBottom: '11%'}}>
-                                {
-                                    perceptronState.perceptron.errorAcumulado != 0 && 
-                                    <ErrorChart />
-                                }
-                                {
-                                    perceptronState.perceptron.errorAcumulado == 0 && 
-                                    <span>No hay información para mostrar</span>
-                                }
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
-            }
+
+
+
         </Container>
     </>
 }
